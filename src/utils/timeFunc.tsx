beb4c1acdd82b9b53efa07dbab015d598639749c
osangel <18515160429@163.com>
2023-03-14 10:39:21 +0800

    fix:fix code err

--- src/utils/timeFunc.tsx
+++ src/utils/timeFunc.tsx
@@ -1,26 +1,22 @@
 const giantSquidStartTimeStr = 'Thu Mar 16 2023 00:00:00 GMT+0800 (中国标准时间)';
 const giantSquidEndTimeStr = 'Fri Mar 31 2023 00:00:00 GMT+0800 (中国标准时间)';
 interface ITimeDuring {
-  (timeStr: string, startTimeStr: string, endTimeStr: string): boolean;
+  (time: Date, startTime: Date, endTime: Date): boolean;
 }
 interface ITimeGiantDuring {
   (): boolean;
 }
-export const giantSquidTimeDuring: ITimeGiantDuring = () => {
-  return judgeDuringTime(
-    new Date().toUTCString(),
-    new Date(giantSquidStartTimeStr).toUTCString(),
-    new Date(giantSquidEndTimeStr).toUTCString(),
+export const getGiantSquidBannerIsActive: ITimeGiantDuring = () => {
+  return getTimeWindowIsActive(
+    new Date(),
+    new Date(giantSquidStartTimeStr),
+    new Date(giantSquidEndTimeStr),
   );
 };
-export const judgeDuringTime: ITimeDuring = (
-  timeStr: string,
-  startTimeStr: string,
-  endTimeStr: string,
-) => {
-  const targetTimeStamp = new Date(timeStr).getTime();
-  const startTimeStamp = new Date(startTimeStr).getTime();
-  const endTimeStamp = new Date(endTimeStr).getTime();
+export const getTimeWindowIsActive: ITimeDuring = (time: Date, startTime: Date, endTime: Date) => {
+  const targetTimeStamp = new Date(time.toUTCString()).getTime();
+  const startTimeStamp = new Date(startTime.toUTCString()).getTime();
+  const endTimeStamp = new Date(endTime.toUTCString()).getTime();
   if (targetTimeStamp <= endTimeStamp && targetTimeStamp >= startTimeStamp) {
     return true;
   }
